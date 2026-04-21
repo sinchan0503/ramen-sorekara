94de05a354f4a2aa7d301f7696ba0efa9ebc919c
澤村　駿 <sawamurashun@sawamurashunnoMacBook-Air.local>
2026-04-21 20:43:13 +0900

    first commit

---
+++ src/content.config.ts
@@ -0,0 +1,13 @@
+import { defineCollection, z } from 'astro:content';
+
+const blog = defineCollection({
+  type: 'content',
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    pubDate: z.date(),
+    emoji: z.string().default('🍜'),
+  }),
+});
+
+export const collections = { blog };
