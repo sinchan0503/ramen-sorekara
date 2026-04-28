aa7c6c8a87c240e8ee30dfe295b5aacd7d4c811a
澤村　駿 <sawamurashun@sawamurashunnoMacBook-Air.local>
2026-04-28 23:10:33 +0900

    fix: use non-www site    url for sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,6 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://www.ramen-sorekara.com',
+  site: 'https://ramen-sorekara.com',
   integrations: [sitemap()],
 });
